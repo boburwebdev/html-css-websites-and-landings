994cfde58d2f13cd49b282eacc1627ac653db2f5
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-09-22 01:05:59 +0500

    finished featured section

--- landing-car-sharing/js/main.js
+++ landing-car-sharing/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.featured__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        infinite: true,
+        fade: true,
+        autoplaySpeed: 2000
+    });
 });
