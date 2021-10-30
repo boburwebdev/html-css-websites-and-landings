75a7e81a1111f3f05c33506a002d975c687a3f21
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-10-30 19:45:28 +0500

    finished gallery section (slick slider)

--- landing-auto-express/js/main.js
+++ landing-auto-express/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.gallery__row').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 });
