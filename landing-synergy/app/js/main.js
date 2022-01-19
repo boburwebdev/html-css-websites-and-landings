3008943be2ee15dc1a7cf76c29867bfe5d5ae76b
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-01-19 19:17:35 +0500

    finished "testimonials"

--- landing-synergy/app/js/main.js
+++ landing-synergy/app/js/main.js
@@ -14,3 +14,20 @@ const projectsSwiper = new Swiper('.projects .swiper', {
     clickable: true
   }
 });
+
+const testimonialsSwiper = new Swiper('.testimonials .swiper', {
+  loop: true,
+  slidesPerView: 1,
+  updateOnWindowResize: true,
+  
+  lazy: {
+    loadPrevNext: true,
+    loadPrevNextAmount: 1
+  },
+
+  pagination: {
+    el: '.testimonials-swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
