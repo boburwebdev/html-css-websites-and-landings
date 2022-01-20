c1f7603bb2273cf649415e2709ed257c8e86c4c2
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-01-21 02:01:53 +0500

    finished "contacts" & "footer"; desktop version is finished

--- landing-synergy/app/js/main.js
+++ landing-synergy/app/js/main.js
@@ -1,3 +1,20 @@
+// Smooth Scroll to "What we do" section from "Hero" section
+const linkHeroArrowDown = document.querySelector(".hero__bottom .hero__arrows-down");
+
+linkHeroArrowDown.addEventListener("click", goToWhatWeDo);
+
+function goToWhatWeDo(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+// Swiper for "Projects"
 const projectsSwiper = new Swiper('.projects .swiper', {
   loop: true,
   slidesPerView: 1,
@@ -15,6 +32,8 @@ const projectsSwiper = new Swiper('.projects .swiper', {
   }
 });
 
+
+// Swiper for "Testimonials";
 const testimonialsSwiper = new Swiper('.testimonials .swiper', {
   loop: true,
   slidesPerView: 1,
@@ -31,3 +50,35 @@ const testimonialsSwiper = new Swiper('.testimonials .swiper', {
     clickable: true
   }
 });
+
+
+// Floating labels for "Contacts" section;
+const contactInputs = document.querySelectorAll('.contacts__form-input');
+const contactTextarea = document.querySelectorAll('.contacts__form-textarea');
+const contactsForm = document.querySelector('.contacts__form');
+
+contactsForm.addEventListener('submit', e => {
+  e.preventDefault()
+});
+
+checkKeyUpEvent(contactInputs);
+checkKeyUpEvent(contactTextarea);
+
+function checkKeyUpEvent(elements) {
+  if (elements) {
+    elements.forEach(element => {
+      element.addEventListener('keyup', addOrRemoveNonEmpty);
+    });
+  }
+}
+
+function addOrRemoveNonEmpty(e) {
+  const currentValue = e.target.value;
+
+    if (currentValue !== '') {
+      e.target.classList.add('not-empty');
+    } else {
+      e.target.classList.remove('not-empty')
+    }
+}
+
