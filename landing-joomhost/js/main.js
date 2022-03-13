a8b8fbfd39df15c6b707df430240f654d78c5b5c
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-13 23:51:07 +0500

    finished media queries

--- landing-joomhost/js/main.js
+++ landing-joomhost/js/main.js
@@ -4,6 +4,19 @@ $(function(){
     slidesToShow: 2,
     slidesToScroll: 2,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 1106,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    ]
   });
+
+  $('.header__hamburger-icon').on('click', function() {
+      $('.header__menu-list').slideToggle();
+  })
 });
