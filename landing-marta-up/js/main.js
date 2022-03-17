33f0c8ba72e65111317cf486fb2bed11375d37f5
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-18 02:34:48 +0500

    finished header and hero; added .vscode to gitignore

--- landing-marta-up/js/main.js
+++ landing-marta-up/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.hero__slider').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'hero__slider-dots',
+        vertical: true,
+        verticalSwiping: true
+    });
 });
