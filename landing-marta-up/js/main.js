80fb84229925a543b56ef962a6ffc350207d2a3d
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-20 03:53:37 +0500

    finished media queries

--- landing-marta-up/js/main.js
+++ landing-marta-up/js/main.js
@@ -1,10 +1,21 @@
 $(function(){
+    
     $('.hero__slider').slick({
         arrows: false,
         dots: true,
         dotsClass: 'hero__slider-dots',
         vertical: true,
         verticalSwiping: true,
-        autoplay: 2000
+        // autoplay: 2000
     });
+
+    $('.header__hamburger').on('click', function() {
+        $('.header__nav-list').toggleClass('show');
+        $('.header__auth-list').removeClass('show');
+    })
+
+    $('.header__auth-icon').on('click', function() {
+        $('.header__nav-list').removeClass('show');
+        $('.header__auth-list').toggleClass('show');
+    })
 });
