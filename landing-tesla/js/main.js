d1facd34da1e0ecd52db7bc69b209b48e03a6e3f
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-21 13:42:00 +0500

    html and css of header and slider are finished; need to add menu;

--- landing-tesla/js/main.js
+++ landing-tesla/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.hero__slider').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'hero__slider-dots',
+        autoplay: 3000,
+        fade: true
+    });
 });
