d9d51835eae35ebe12eb9be0cdb6dae909869b20
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-21 18:45:05 +0500

    finished menu

--- landing-tesla/js/main.js
+++ landing-tesla/js/main.js
@@ -6,4 +6,12 @@ $(function(){
         autoplay: 3000,
         fade: true
     });
+
+    $('.header__hamburger').on('click', function() {
+        $('.header__nav').slideToggle();
+    });
+    
+    $('.header__nav-close').on('click', function() {
+        $('.header__nav').slideToggle();
+    })
 });
