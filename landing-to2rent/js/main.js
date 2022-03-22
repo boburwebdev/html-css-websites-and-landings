58f6442938b2d947929a347943bb5e84d6c1bbe1
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-23 01:52:32 +0500

    added slick js and finished slider

--- landing-to2rent/js/main.js
+++ landing-to2rent/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.slider').slick({
+        prevArrow: `<button class="slick-arrow slick-prev"><img src="../images/slider-arrow-left.svg" alt="arrow-left"></button>`,
+        nextArrow: `<button class="slick-arrow slick-next"><img src="../images/slider-arrow-right.svg" alt="arrow-right"></button>`,
+    })
 });
+
