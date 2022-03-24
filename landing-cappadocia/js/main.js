f8344c25cbaaaa62657d8fd3ae70cbe3f5d6d70e
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-25 03:55:56 +0500

    finished "fairy-tale" and "our-trip" sections

--- landing-cappadocia/js/main.js
+++ landing-cappadocia/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
+    $('.fairy-tale__slider').slick({
+        prevArrow: `<button class="slick-arrow slick-arrow--prev" type="button"><img class="slick-arrow__image" src="images/arrow-prev.svg" alt="prev" /></button>`,
+        nextArrow: `<button class="slick-arrow slick-arrow--next" type="button"><img class="slick-arrow__image" src="images/arrow-next.svg" alt="prev" /></button>`,
+        autoplay: true,
+    });
+
+    $('.our-trip__slider').slick({
+        prevArrow: `<button class="slick-arrow slick-arrow--prev" type="button"><img class="slick-arrow__image" src="images/arrow-prev.svg" alt="prev" /></button>`,
+        nextArrow: `<button class="slick-arrow slick-arrow--next" type="button"><img class="slick-arrow__image" src="images/arrow-next.svg" alt="prev" /></button>`,
+        autoplay: true,
+    });
+
 
 });
