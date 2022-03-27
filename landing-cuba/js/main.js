445f70a41d7b05a2a36fb7fd9bf1101370049a0d
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-27 22:24:33 +0500

    finished "photos" section

--- landing-cuba/js/main.js
+++ landing-cuba/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+    $('.photos__slider').slick({
+        arrows: true,
+        prevArrow: `<button class="slick-prev slick-arrow"><img class="slick-arrow-img slick-arrow-img--left" src="images/arrow-left.svg" alt="left"></button>`,
+        nextArrow: `<button class="slick-next slick-arrow"><img class="slick-arrow-img slick-arrow-img--right" src="images/arrow-right.svg" alt="right"></button>`,
+        autoplay: true,
+    });
 });
