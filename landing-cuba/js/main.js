8b22ac939b7a45eed796ef0f5bbf23cbdeb95969
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-28 12:15:22 +0500

    finished media queries

--- landing-cuba/js/main.js
+++ landing-cuba/js/main.js
@@ -5,4 +5,8 @@ $(function(){
         nextArrow: `<button class="slick-next slick-arrow"><img class="slick-arrow-img slick-arrow-img--right" src="images/arrow-right.svg" alt="right"></button>`,
         autoplay: true,
     });
+
+    $('.menu__hamburger').on('click', function() {
+        $('.menu__list').slideToggle();
+    })
 });
