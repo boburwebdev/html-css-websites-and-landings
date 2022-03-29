26faa1f3ccefd2ef7ceba3cf372179856932322e
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-03-30 01:58:44 +0500

    finished "heroes" section (slider syncing)

--- website-vikings/js/main.js
+++ website-vikings/js/main.js
@@ -1,8 +1,27 @@
 $(function(){
+
     $('[data-fancybox]').fancybox({
         youtube: {
             controls: 0,
             showinfo: 0
         }
-    })
+    });
+
+    $('.heroes__image-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__info-slider',
+        prevArrow: `<button class="heroes__image-slider-arrow heroes__image-slider-arrow--prev slick-prev"><img class="slick-arrow-image slick-arrow-image--left" src="images/slick-arrow-left.png" alt="left"></button>`,
+        nextArrow: `<button class="heroes__image-slider-arrow heroes__image-slider-arrow--next slick-next"><img class="slick-arrow-image slick-arrow-image--right" src="images/slick-arrow-right.png" alt="right"></button>`
+    });
+
+    $('.heroes__info-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__image-slider',
+        fade: true,
+        arrows: false
+    });
+	
+
 });
