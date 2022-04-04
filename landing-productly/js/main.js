7f5bc24fd12a6996727762904d55b489758166b6
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-04 15:05:45 +0500

    finished media queries

--- landing-productly/js/main.js
+++ landing-productly/js/main.js
@@ -4,6 +4,32 @@ $(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     dots: true,
-    dotsClass: 'blog__slider-dots'
+    dotsClass: 'blog__slider-dots',
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
+  $('.menu__hamburger').on('click', function() {
+    $('.menu__list').toggleClass('show');
+    $('.header__auth-list').removeClass('show');
+  })
+  
+  $('.header__auth-icon').on('click', function() {
+    $('.menu__list').removeClass('show');
+    $('.header__auth-list').toggleClass('show');
+  })
 });
