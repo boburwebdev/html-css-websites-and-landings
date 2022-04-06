d6875ef8fe5044f3122c4a1111c49c8ad1fce67e
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-06 15:43:45 +0500

    finished "info-slider" section

--- landing-cosmic-tourism/js/main.js
+++ landing-cosmic-tourism/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+  $('.info__slider').slick({
+    arrows: true,
+    prevArrow: `<button class="slick-btn slick-btn--prev" type="button"><img class="slick-arrow slick-arrow--prev" src="images/info__slider-arrow--prev.svg" alt="prev"></button>`,
+    nextArrow: `<button class="slick-btn slick-btn--next" type="button"><img class="slick-arrow slick-arrow--next" src="images/info__slider-arrow--next.svg" alt="next"></button>`,
+  });
 });
