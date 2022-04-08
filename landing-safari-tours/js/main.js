5099d1b8a6840f7d71b5218e1e09e3e3a6c595ab
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-08 23:07:54 +0500

    finished "about" section

--- landing-safari-tours/js/main.js
+++ landing-safari-tours/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+  $('.about__video').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
+		fixedContentPos: false
+	});
 });
