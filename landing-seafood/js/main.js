8faff1905016da2cc0ecc46968327406d58c9cef
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-12 16:51:46 +0500

    finished "hero" section

--- landing-seafood/js/main.js
+++ landing-seafood/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+  $('.hero__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    fade: true,
+    prevArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--prev"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-prev.svg" alt="prev" ></button>`,
+    nextArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--next"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-next.svg" alt="next" ></button>`
+  })
 });
