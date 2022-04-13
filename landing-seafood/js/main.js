81148c55425e8ac16f5e253d6e8aad032fa728e4
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-13 23:13:06 +0500

    finished "products" section

--- landing-seafood/js/main.js
+++ landing-seafood/js/main.js
@@ -6,5 +6,31 @@ $(function(){
     fade: true,
     prevArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--prev"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-prev.svg" alt="prev" ></button>`,
     nextArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--next"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-next.svg" alt="next" ></button>`
-  })
+  });
+
+  // $('.products__name').slick({
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,
+  //   vertical: true,
+  //   centerMode: true,
+  // });
+
+  $('.products__name').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.products__content',
+    vertical: true,
+    centerMode: true,
+    focusOnSelect: true,
+    prevArrow: `<button type="button" class="products__slider-arrow products__slider-arrow--prev"><img class="products__slider-arrow-image" src="images/products-slider-arrow-prev.svg" alt="prev" ></button>`,
+    nextArrow: `<button type="button" class="products__slider-arrow products__slider-arrow--next"><img class="products__slider-arrow-image" src="images/products-slider-arrow-next.svg" alt="next" ></button>`
+  });
+
+  $('.products__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.products__name'
+  });
 });
