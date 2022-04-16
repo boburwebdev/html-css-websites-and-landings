68c4eeac2156d4a16fd874fc8b272f489b58bd95
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-16 22:04:08 +0500

    finished media queries

--- landing-seafood/js/main.js
+++ landing-seafood/js/main.js
@@ -5,15 +5,17 @@ $(function(){
     dots: true,
     fade: true,
     prevArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--prev"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-prev.svg" alt="prev" ></button>`,
-    nextArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--next"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-next.svg" alt="next" ></button>`
+    nextArrow: `<button type="button" class="hero__slider-arrow hero__slider-arrow--next"><img class="hero__slider-arrow-image" src="images/hero-slider-arrow-next.svg" alt="next" ></button>`,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          fade: false,
+        }
+      },
+    ]
   });
-
-  // $('.products__name').slick({
-  //   slidesToShow: 6,
-  //   slidesToScroll: 1,
-  //   vertical: true,
-  //   centerMode: true,
-  // });
 
   $('.products__name').slick({
     slidesToShow: 4,
@@ -23,7 +25,40 @@ $(function(){
     centerMode: true,
     focusOnSelect: true,
     prevArrow: `<button type="button" class="products__slider-arrow products__slider-arrow--prev"><img class="products__slider-arrow-image" src="images/products-slider-arrow-prev.svg" alt="prev" ></button>`,
-    nextArrow: `<button type="button" class="products__slider-arrow products__slider-arrow--next"><img class="products__slider-arrow-image" src="images/products-slider-arrow-next.svg" alt="next" ></button>`
+    nextArrow: `<button type="button" class="products__slider-arrow products__slider-arrow--next"><img class="products__slider-arrow-image" src="images/products-slider-arrow-next.svg" alt="next" ></button>`,
+    responsive: [
+      {
+        breakpoint: 1061,
+        settings: {
+          slidesToShow: 3,
+          vertical: false,
+        }
+      },
+      {
+        breakpoint: 961,
+        settings: {
+          slidesToShow: 3,
+          centerMode: false,
+          vertical: false,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+          vertical: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          vertical: false,
+        }
+      },
+    ]
   });
 
   $('.products__content').slick({
@@ -31,6 +66,10 @@ $(function(){
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.products__name'
+    asNavFor: '.products__name',
   });
+
+  $('.menu__hamburger').on('click', function() {
+    $('.menu__list').slideToggle();
+  })
 });
