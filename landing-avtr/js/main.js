7f1072443c7061ef09b23546abd16ffd994fa38e
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-19 01:22:01 +0500

    finished "design__slider" section

--- landing-avtr/js/main.js
+++ landing-avtr/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $('.design__slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow: `<button class="design__slider-arrow design__slider-arrow--prev" type="button"><img class="design__slider-arrow--img" src="images/design__slider-arrow--prev.svg" alt="prev"></button>`,
+    nextArrow: `<button class="design__slider-arrow design__slider-arrow--next" type="button"><img class="design__slider-arrow--img" src="images/design__slider-arrow--next.svg" alt="next"></button>`
+  })
 });
