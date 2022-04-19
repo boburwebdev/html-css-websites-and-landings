98a756c12e13b6db44fb074ca277d803e101cb4d
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-19 11:20:00 +0500

    finished media queries

--- landing-avtr/js/main.js
+++ landing-avtr/js/main.js
@@ -4,6 +4,17 @@ $(function(){
     slidesToShow: 4,
     variableWidth: true,
     prevArrow: `<button class="design__slider-arrow design__slider-arrow--prev" type="button"><img class="design__slider-arrow--img" src="images/design__slider-arrow--prev.svg" alt="prev"></button>`,
-    nextArrow: `<button class="design__slider-arrow design__slider-arrow--next" type="button"><img class="design__slider-arrow--img" src="images/design__slider-arrow--next.svg" alt="next"></button>`
+    nextArrow: `<button class="design__slider-arrow design__slider-arrow--next" type="button"><img class="design__slider-arrow--img" src="images/design__slider-arrow--next.svg" alt="next"></button>`,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          centerMode: true,
+          arrows: false,
+        }
+      }
+    ]
   })
 });
