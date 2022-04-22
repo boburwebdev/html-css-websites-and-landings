f087e38f6c38ae5c34069250481db99d63f9cbf0
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-23 01:57:04 +0500

    finished header-slider

--- landing-gosurf/app/js/main.js
+++ landing-gosurf/app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
+  $('.header__slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: `<button class="slider-arrow header__slider-arrow header__slider-arrow--prev"><img class="slider-arrow-img" src="img/arrow-left.svg" alt="prev"></button>`,
+    nextArrow: `<button class="slider-arrow header__slider-arrow header__slider-arrow--next"><img class="slider-arrow-img" src="img/arrow-right.svg" alt="next"></button>`,
+    asNavFor: '.header__slider-nav'
+  });
 
-
+  $('.header__slider-nav').slick({
+    slidesToShow: 4,
+    arrows: false,
+    asNavFor: '.header__slider',
+    focusOnSelect: true
+  })
   
 });
