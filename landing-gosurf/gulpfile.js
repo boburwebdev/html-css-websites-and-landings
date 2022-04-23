bc2b414aacd1ff9e1d4869bdd723e9f65360aadc
boburwebdev <boburjon.zokirov.2017@gmail.com>
2022-04-23 09:47:47 +0500

    finished header-aside

--- landing-gosurf/gulpfile.js
+++ landing-gosurf/gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('clean', async function(){
 
 gulp.task('scss', function(){
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({outputStyle: 'expanded'}))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 8 versions']
     }))
