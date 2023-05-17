dd198005a5f62068d9cff4b18cfe226cd1ff3df5
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-05-17 13:02:32 +0500

    xiaomi-himo: finished header and hero

---
+++ landing-xiaomi-himo/app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.hero__slider').slick({
+    arrows: false,
+    dots: true,
+    vertical: true,
+    verticalSwiping: true,
+    autoplay: true,
+    pauseOnHover: true
+  });
+});
