7ead585e8c0c76ede4edad07a1687759dbb75751
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-08-02 23:54:30 +0500

    landing-oculus: finished media queries

--- landing-oculus/app/js/main.js
+++ landing-oculus/app/js/main.js
@@ -9,4 +9,8 @@ featuresTabHeaders.forEach((headerItem, idx) => {
     e.currentTarget.classList.add("active");
     featuresTabBodyItems[idx].classList.add("active");
   });
-})
+});
+
+const menuHamburger = document.querySelector(".header .nav .nav__menu-hamburger");
+const headerNavMenu = document.querySelector(".header .nav__menu");
+menuHamburger.addEventListener("click", () => { headerNavMenu.classList.toggle("active") });
