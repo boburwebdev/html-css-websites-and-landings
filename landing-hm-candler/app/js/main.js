ff7ead367a2e354a1e38685e294f4b4685af450b
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-08-13 02:45:33 +0500

    landing-hm-candler: finished "certificates" section

--- landing-hm-candler/app/js/main.js
+++ landing-hm-candler/app/js/main.js
@@ -5,14 +5,35 @@ headerMenuHamburger.addEventListener("click", () => {
   headerNavMenu.classList.toggle("active");
 });
 
-const swiper = new Swiper('.feedbacks__slider', {
+const swiperFeedbacks = new Swiper('.feedbacks__slider', {
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination--feedbacks',
     clickable: true
   },
   autoplay: {
     delay: 3000,
     disableOnInteraction: true,
   },
+});
+
+const swiperCertificates = new Swiper('.certificates__slider', {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination--certificates',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: "auto"
+    },
+    768: {
+      spaceBetween: 20
+    }
+  }
 });
