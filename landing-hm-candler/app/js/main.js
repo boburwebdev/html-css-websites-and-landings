78f2c56cb6ea8191988adea63fe6dd380c349693
boburwebdev <boburjon.zokirov.2017@gmail.com>
2023-08-12 23:45:38 +0500

    landing-hm-candler: finished "feedback" section

--- landing-hm-candler/app/js/main.js
+++ landing-hm-candler/app/js/main.js
@@ -3,4 +3,16 @@ const headerNavMenu = document.querySelector(".header .nav__menu");
 
 headerMenuHamburger.addEventListener("click", () => {
   headerNavMenu.classList.toggle("active");
-})
+});
+
+const swiper = new Swiper('.feedbacks__slider', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+});
